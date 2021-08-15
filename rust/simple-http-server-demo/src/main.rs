8e9742f37b8ab5681359384543b61935eeb2bada
tabakazu <tkazz.bd1@gmail.com>
2021-08-16 00:41:37 +0900

    add simple response to stream

--- rust/simple-http-server-demo/src/main.rs
+++ rust/simple-http-server-demo/src/main.rs
@@ -2,21 +2,20 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
 
-// http://localhost:7878/
+// curl http://localhost:7878/
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
         handle_connection(stream);
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-
-    stream.read(&mut buffer).unwrap();
-
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let response = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>Hello world</body></html>\r\n";
+    match stream.write(response) {
+        Ok(_) => println!("Response sent"),
+        Err(e) => println!("Failed sending response: {}", e),
+    }
 }
